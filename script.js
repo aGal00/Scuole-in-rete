5ac0bd8ddfa8d487eb9a975c533fca3f7a3ef89f
leonardoSparano <117900747+leonardoSparano@users.noreply.github.com>
2024-05-03 06:38:31 +0000

    nuove keys database

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* function ricerca() { // link del database
-    fetch('https://congenial-trout-56g5wvw6px6f47v4-1337.app.github.dev/')
+    fetch('https://vigilant-fortnight-9v7wp6pvwgwh7qg6-1337.app.github.dev/')
     .then(response => response.json())
     .then(response => {
         console.log(response);
