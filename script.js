dc819f741a0d59f6a0175b8453eb6eb38a028d8f
leonardoSparano <117900747+leonardoSparano@users.noreply.github.com>
2024-05-03 07:53:44 +0000

    richieste fetch fatte

--- script.js
+++ script.js
@@ -1,11 +1,144 @@
-function ricerca(endpoint) { // link del database
-    fetch('https://vigilant-fortnight-9v7wp6pvwgwh7qg6-1337.app.github.dev/' + endpoint)
+function getBranches() {
+    fetch('https://vigilant-fortnight-9v7wp6pvwgwh7qg6-1337.app.github.dev/api/branches')
         .then(response => response.json())
         .then(response => {
-            if (response) {
-                return response;
+            if (response.data) {
+                console.log(response.data)
             } else {
-                return "error";
+                console.log("no data")
+            }
+        })
+}
+
+function getBranch(id) {
+    fetch('https://vigilant-fortnight-9v7wp6pvwgwh7qg6-1337.app.github.dev/api/branches/'+id)
+        .then(response => response.json())
+        .then(response => {
+            if (response.data) {
+                console.log(response.data)
+            } else {
+                console.log("no data")
+            }
+        })
+}
+
+function getInstitutes() {
+    fetch('https://vigilant-fortnight-9v7wp6pvwgwh7qg6-1337.app.github.dev/api/institutes')
+        .then(response => response.json())
+        .then(response => {
+            if (response.data) {
+                console.log(response.data)
+            } else {
+                console.log("no data")
+            }
+        })
+}
+
+function getInstitute(id) {
+    fetch('https://vigilant-fortnight-9v7wp6pvwgwh7qg6-1337.app.github.dev/api/institutes/'+id)
+        .then(response => response.json())
+        .then(response => {
+            if (response.data) {
+                console.log(response.data)
+            } else {
+                console.log("no data")
+            }
+        })
+}
+
+function getOpenDays() {
+    fetch('https://vigilant-fortnight-9v7wp6pvwgwh7qg6-1337.app.github.dev/api/open-days')
+        .then(response => response.json())
+        .then(response => {
+            if (response.data) {
+                console.log(response.data)
+            } else {
+                console.log("no data")
+            }
+        })
+}
+
+function getOpenDay(id) {
+    fetch('https://vigilant-fortnight-9v7wp6pvwgwh7qg6-1337.app.github.dev/api/open-days/'+id)
+        .then(response => response.json())
+        .then(response => {
+            if (response.data) {
+                console.log(response.data)
+            } else {
+                console.log("no data")
+            }
+        })
+}
+
+function getPaths() {
+    fetch('https://vigilant-fortnight-9v7wp6pvwgwh7qg6-1337.app.github.dev/api/paths')
+        .then(response => response.json())
+        .then(response => {
+            if (response.data) {
+                console.log(response.data)
+            } else {
+                console.log("no data")
+            }
+        })
+}
+
+function getPath(id) {
+    fetch('https://vigilant-fortnight-9v7wp6pvwgwh7qg6-1337.app.github.dev/api/paths/'+id)
+        .then(response => response.json())
+        .then(response => {
+            if (response.data) {
+                console.log(response.data)
+            } else {
+                console.log("no data")
+            }
+        })
+}
+
+function getSchools() {
+    fetch('https://vigilant-fortnight-9v7wp6pvwgwh7qg6-1337.app.github.dev/api/schools')
+        .then(response => response.json())
+        .then(response => {
+            if (response.data) {
+                console.log(response.data)
+            } else {
+                console.log("no data")
+            }
+        })
+}
+
+function getSchool(id) {
+    fetch('https://vigilant-fortnight-9v7wp6pvwgwh7qg6-1337.app.github.dev/api/schools/'+id)
+        .then(response => response.json())
+        .then(response => {
+            if (response.data) {
+                console.log(response.data)
+            } else {
+                console.log("no data")
+            }
+        })
+}
+
+
+function getActivities() {
+    fetch('https://vigilant-fortnight-9v7wp6pvwgwh7qg6-1337.app.github.dev/api/activities')
+        .then(response => response.json())
+        .then(response => {
+            if (response.data) {
+                console.log(response.data)
+            } else {
+                console.log("no data")
+            }
+        })
+}
+
+function getActivity(id) {
+    fetch('https://vigilant-fortnight-9v7wp6pvwgwh7qg6-1337.app.github.dev/api/activities/'+id)
+        .then(response => response.json())
+        .then(response => {
+            if (response.data) {
+                console.log(response.data)
+            } else {
+                console.log("no data")
             }
         })
 }
