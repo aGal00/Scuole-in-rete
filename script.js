ee0d305359a5bba40dd1274284155c08e2d58b1d
leonardoSparano <117900747+leonardoSparano@users.noreply.github.com>
2024-05-14 13:15:50 +0000

    finito script

--- script.js
+++ script.js
@@ -106,27 +106,11 @@ function getSchools() {
         })
 }
 
-function getSchool(id) {
-    fetch('https://redesigned-eureka-r46pgjq57w62x765-1337.app.github.dev/api/schools/' + id + '?populate=*')
-        .then(response => response.json())
-        .then(response => {
-            if (response.data) {
-                console.log(response.data)
-                let maindiv = document.getElementById('scuole');
-                let table = createSchoolTable(response.data);
-                if (maindiv.lastChild) {
-                    maindiv.removeChild(maindiv.lastChild);
-                }
-                maindiv.appendChild(table);
-            } else {
-                console.log("no data")
-            }
-        })
-}
+
 
 
 function getActivities() {
-    fetch('https://redesigned-eureka-r46pgjq57w62x765-1337.app.github.dev/api/activities')
+    fetch('https://vigilant-fortnight-9v7wp6pvwgwh7qg6-1337.app.github.dev/api/activities')
         .then(response => response.json())
         .then(response => {
             if (response.data) {
@@ -141,7 +125,7 @@ function getActivities() {
 }
 
 function getActivity(id) {
-    fetch('https://redesigned-eureka-r46pgjq57w62x765-1337.app.github.dev/api/activities/' + id + '?populate=*')
+    fetch('https://vigilant-fortnight-9v7wp6pvwgwh7qg6-1337.app.github.dev/api/activities/' + id + '?populate=*')
         .then(response => response.json())
         .then(response => {
             if (response.data) {
@@ -178,65 +162,20 @@ function createSchoolsList(schools) {
     let table = document.createElement('table');
     schools.forEach(school => {
         console.log(school)
-        let row = document.createElement('tr')
-        row.onclick = function () {
-            getSchool(school.id);
-        }
-        let cell = document.createElement('td')
-        cell.classList.add('tabella-scuole');
-        cell.innerText = school.attributes.Name;
-        row.appendChild(cell);
-        cell = document.createElement('td')
-        cell.classList.add('tabella-scuole');
-        cell.innerText = school.attributes.City;
-        row.appendChild(cell);
-        cell = document.createElement('td')
-        cell.classList.add('tabella-scuole');
-        cell.innerText = school.attributes.Email;
-        row.appendChild(cell);
-        table.appendChild(row);
+        let a = document.createElement('a')
+        let link = './scuola.html?id=' + school.id
+        a.setAttribute("href", link)
+        let div = document.createElement('div')
+        let p = document.createElement('div')
+        p.innerText = school.attributes.Name
+        div.appendChild(p)
+        a.appendChild(div)
+        table.appendChild(a);
     });
     return table;
 }
 
-function createSchoolTable(school) {
-    let infos = school.attributes;
-    console.log(infos);
-    let div = document.createElement('div')
-    let name = document.createElement('h1')
-    name.innerText = infos.Name
-    div.appendChild(name)
 
-    let address = document.createElement('h1')
-    address.innerText = infos.Address
-    div.appendChild(address)
-
-    let h1 = document.createElement('h1');
-    h1.innerText = infos.CAP
-    div.appendChild(h1)
-
-    h1 = document.createElement('h1');
-    h1.innerText = infos.Province
-    div.appendChild(h1)
-
-    h1 = document.createElement('h1');
-    h1.innerText = infos.Website
-    div.appendChild(h1)
-
-    h1 = document.createElement('h1');
-    h1.innerText = infos.Phone
-    div.appendChild(h1)
-
-    h1 = document.createElement('h1');
-    h1.innerText = infos.Fax
-    div.appendChild(h1)
-
-    h1 = document.createElement('h1');
-    h1.innerText = infos.PEC
-    div.appendChild(h1)
-
-    return div;
-}
 
 function start() {
     const myKeyValues = window.location.search;
@@ -253,9 +192,6 @@ function start() {
             } else {
                 console.log("no data")
             }
-
-
-
         })
 }
 
